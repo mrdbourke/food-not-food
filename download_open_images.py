e88e219c7b0a8cbd346b854a2551f493fda64546
Daniel Bourke <danielbourke@icloud.com>
2021-12-08 17:35:31 +1000

    add open images download script

--- download_open_images.py
+++ download_open_images.py
@@ -6,22 +6,48 @@ Uses FiftyOne: https://voxel51.com/docs/fiftyone/tutorials/open_images.html
 import fiftyone as fo
 import fiftyone.zoo as foz
 import os
+import argparse
 
-targ_dir = "data/open_images"
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-t",
+    "--targ_dir",
+    default="data/open_images",
+    help="target directory to download images to",
+)
+parser.add_argument(
+    "-s", "--seed", default=42, help="random seed for downloading images"
+)
+parser.add_argument(
+    "-n",
+    "--num_samples",
+    default=100,
+    help="number of images to download from Open Images (downloads occur from train, val, test sets by default)",
+)
 
-if os.path.exists(targ_dir):
-    pass
-else:
+args = parser.parse_args()
+
+targ_dir = args.targ_dir
+num_samples = args.num_samples
+seed = args.seed
+
+# Print what's happening
+print(f"Downloading {num_samples} images to {targ_dir} with seed: {seed}...")
+
+# Create Target Directly
+if not os.path.exists(targ_dir):
     os.makedirs(targ_dir)
 
+# Download images from Open Images
 dataset = foz.load_zoo_dataset(
     "open-images-v6",
     split=None,  # use split=None to download from all 3 splits (train, val, test)
     label_types=["classifications"],  # only get classification level labels
-    max_samples=100,
-    seed=42,  # set seed to download same images each time
+    max_samples=num_samples,
+    seed=seed,  # set seed to download same images each time
     shuffle=True,
     dataset_dir=targ_dir,
 )
 
-session = fo.launch_app(dataset)
+# Launch an iteractive data session with Voxel51's data explorer
+# session = fo.launch_app(dataset)
