47ca3aba2ed28aec5b83df4f45ea2ae91bd08999
Daniel Bourke <danielbourke@icloud.com>
2022-03-18 17:15:10 +1000

    update model training code

--- model_building/train_model.py
+++ model_building/train_model.py
@@ -1,11 +1,13 @@
 import os
 import tensorflow as tf
 import argparse
+import logging
 
-print(tf.__version__)
+print(f"[INFO] TensorFlow version: {tf.__version__}")
 
 assert tf.__version__.startswith("2")
-tf.get_logger().setLevel("ERROR")
+# Filter out messages, see here: https://stackoverflow.com/a/38645250/7900723
+tf.get_logger().setLevel(logging.ERROR)
 
 from pathlib import Path
 from datetime import date
@@ -29,6 +31,11 @@ parser.add_argument(
 parser.add_argument(
     "--use_data_aug", default=True, help="Whether to use data augmentation or not."
 )
+parser.add_argument(
+    "--model_spec",
+    default="efficientnet_lite0",
+    help="Which efficientnet-liteX model to use, X can be 0, 1, 2, 3, 4",
+)
 args = parser.parse_args()
 
 # Setup parser variables
@@ -36,6 +43,7 @@ train_data_path = args.train_dir
 test_data_path = args.test_dir
 export_dir = Path(args.export_dir)
 NUM_EPOCHS = int(args.epochs)
+MODEL_SPEC = args.model_spec
 
 
 # Find class names
@@ -48,8 +56,12 @@ test_data = DataLoader.from_folder(test_data_path)
 
 # Create model
 print(f"[INFO] Creating and training model...")
+print(f"[INFO] Training {MODEL_SPEC} for {NUM_EPOCHS} epochs...")
 model = image_classifier.create(
-    train_data=train_data, epochs=NUM_EPOCHS, use_augmentation=args.use_data_aug
+    train_data=train_data,
+    model_spec=MODEL_SPEC,
+    epochs=NUM_EPOCHS,
+    use_augmentation=args.use_data_aug,
 )
 
 # Evaluate model
@@ -68,9 +80,10 @@ model_number = get_model_number(export_dir)
 # Get current date
 current_date = str(date.today())
 model_save_path = Path(
-    export_dir, f"{current_date}_food_not_food_model_v{model_number}.tflite"
+    export_dir,
+    f"{current_date}_food_not_food_model_{MODEL_SPEC}_v{model_number}.tflite",
 )
 
 print(f"[INFO] Saving the model to '{export_dir}' directory as '{model_save_path}'...")
 model.export(export_dir=export_dir, tflite_filename=model_save_path)
-print(f"[INFO] Model saved.")
+print(f"[INFO] Model saved to: '{model_save_path}'.")
